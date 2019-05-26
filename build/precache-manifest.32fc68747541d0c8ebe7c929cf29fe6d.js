8ffb4dc207724e903abddc10f268e467a929f826
Digvijay Upadhyay <digvijay.upadhyay@globant.com>
2019-05-27 00:30:54 +0100

    build folder to host on pages

---
+++ build/precache-manifest.32fc68747541d0c8ebe7c929cf29fe6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0bf2bda885332416522ba52831e5173",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34a3bdb32f5d4ec5a52c",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "079ba9f558dcab000a49",
+    "url": "/static/js/2.ccc3d67c.chunk.js"
+  },
+  {
+    "revision": "34a3bdb32f5d4ec5a52c",
+    "url": "/static/js/main.5b14e1f0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
